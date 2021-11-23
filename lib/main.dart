ab567588a081cf3f2cc2abcf48162ccde06e0ddc
Pertempto <pertemptodisce@gmail.com>
2021-11-22 22:40:09 -0500

    Got maps working.

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:platform_maps_flutter/platform_maps_flutter.dart';
 
 import 'data/data_store.dart';
 
 void main() {
   DataStore.init();
+  if (defaultTargetPlatform == TargetPlatform.android) {
+    AndroidGoogleMapsFlutter.useAndroidViewSurface = true;
+  }
   runApp(const MyApp());
 }
 
@@ -42,6 +48,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Position? position;
+  final Completer<GoogleMapController> _controller = Completer();
 
   @override
   Widget build(BuildContext context) {
@@ -49,49 +56,11 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: const Text('Pins'),
       ),
-      body: PlatformMap(
-        initialCameraPosition: CameraPosition(
-          target: const LatLng(47.6, 8.8796),
-          zoom: 16.0,
-        ),
-        markers: Set<Marker>.of(
-          [
-            Marker(
-              markerId: MarkerId('marker_1'),
-              position: LatLng(47.6, 8.8796),
-              consumeTapEvents: true,
-              infoWindow: InfoWindow(
-                title: 'PlatformMarker',
-                snippet: "Hi I'm a Platform Marker",
-              ),
-              onTap: () {
-                print("Marker tapped");
-              },
-            ),
-          ],
-        ),
-        mapType: MapType.satellite,
-        onTap: (location) => print('onTap: $location'),
-        onCameraMove: (cameraUpdate) => print('onCameraMove: $cameraUpdate'),
-        compassEnabled: true,
-        onMapCreated: (controller) {
-          Future.delayed(Duration(seconds: 2)).then(
-                (_) {
-              controller.animateCamera(
-                CameraUpdate.newCameraPosition(
-                  const CameraPosition(
-                    bearing: 270.0,
-                    target: LatLng(51.5160895, -0.1294527),
-                    tilt: 30.0,
-                    zoom: 18,
-                  ),
-                ),
-              );
-              controller
-                  .getVisibleRegion()
-                  .then((bounds) => print("bounds: ${bounds.toString()}"));
-            },
-          );
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        initialCameraPosition: const CameraPosition(target: LatLng(39.75, -84.20), zoom: 12), // initial: Dayton
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
         },
       ),
       floatingActionButton: FloatingActionButton(
@@ -106,9 +75,18 @@ class _MyHomePageState extends State<MyHomePage> {
     DataStore.determinePosition().then((pos) {
       setState(() {
         position = pos;
+        _updateView();
       });
     }, onError: (error) {
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text('ERROR: $error')));
     });
+  }
+
+  Future<void> _updateView() async {
+    if (position != null) {
+      final GoogleMapController controller = await _controller.future;
+      controller.animateCamera(CameraUpdate.newCameraPosition(
+          CameraPosition(bearing: 0, target: LatLng(position!.latitude, position!.longitude), zoom: 19)));
+    }
   }
 }
